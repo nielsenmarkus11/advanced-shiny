efce9b882469fa9a42850373db5bc22fd314b4c7
Mark Nielsen <nielsen.markus11@gmail.com>
2021-06-02 21:42:50 +0000

    Add terminal and console code.

---
+++ code-from-workshop.R
@@ -0,0 +1,11 @@
+library(usethis)
+use_git_config(user.name = "Mark Nielsen", user.email = "nielsen.markus11@gmail.com")
+
+install.packages('blastula')
+
+renv::restore()
+
+usethis::edit_r_environ() # Opens .Renviron file for project
+
+shinytest::installDependencies()
+shinytest::testApp(suffix = "linux")
